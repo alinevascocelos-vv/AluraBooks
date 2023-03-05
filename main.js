890b63b435c164db26a05f01a9905a86e5f4992b
Aline Vasconcelos <87406680+alinevascocelos-vv@users.noreply.github.com>
2023-03-05 12:30:32 -0300

    Conclusão do projeto

---
+++ main.js
@@ -0,0 +1,11 @@
+ src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"
+    
+        const swiper = new Swiper('.swiper', {
+        spaceBetween: 10,
+        slidesPerView: 3,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+  },
+});
+    
